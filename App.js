6b093878bc499334dd6f209c4695dd70252a7223
anyachan1503 <125202830+anyachan1503@users.noreply.github.com>
2023-08-14 22:21:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <Text style={{marginTop:50,color: 'white',
+	textAlign: 'center', 
+	fontWeight: 'bold',
+	fontSize: 50,
+	backgroundColor: 'pink',width: 300,
+  height: 300,
+  /*Add the border radius for left right of top and bottom*/
+  borderBottomLeftRadius:30,
+  borderTopLeftRadius:30,
+ /*Add font as Times New Roman*/  
+  fontStyle:"TimesNewRoman",
+    /*Add the shadowcolor */
+    textShadowColor:'black',
+    
+  textShadowOffset:{width: 10, height: 10},
+    textShadowRadius:8,
+    }}>HAPPY BIRTHDAY MY DEAR FRIEND:)</Text>
+  
+    );
+  }
+}
+
+
